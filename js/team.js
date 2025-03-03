a638f3b6a98576c86569ef01c292cf0e37dc629f
=Mclee644 <644mcl@gmail.com>
2025-03-03 20:18:57 +0000

    converting project from react to regular hmtl, css, js

---
+++ js/team.js
@@ -0,0 +1,33 @@
+const team = [
+  {
+    id: 1,
+    name: "Caleb",
+    role: "Project Manager",
+  },
+  {
+    id: 2,
+    name: "Bright Mensah Obeng",
+    role: "Front-end Developer & Web Designer",
+  },
+  {
+    id: 3,
+    name: "Richard",
+    role: "Front-end Developer",
+  },
+  {
+    id: 4,
+    name: "Stephan",
+    role: "Web Designer",
+  },
+  {
+    id: 5,
+    name: "Stephan x",
+    role: "Web Designer",
+  },
+  {
+    id: 6,
+    name: "Stephan y",
+    role: "Web Designer",
+  },
+];
+export default team;
